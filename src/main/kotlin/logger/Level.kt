8b4cc5707a9dbbde6f2aa5a5b1979f51037cd8fe
Stan <stan@stanroelofs.nl>
2021-02-28 11:04:36 +0100

    Added logging level enum

---
+++ src/main/kotlin/logger/Level.kt
@@ -0,0 +1,12 @@
+package logger
+
+enum class Level(private val stringValue: String) {
+    DEBUG("DEBUG"),
+    INFO("INFO"),
+    WARNING("WARNING"),
+    ERROR("ERROR");
+
+    override fun toString(): String {
+        return stringValue
+    }
+}

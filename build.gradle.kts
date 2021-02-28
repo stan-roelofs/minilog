14635a4f3c051611a3abfcb5af9d8926369520da
Stan <stan@stanroelofs.nl>
2021-02-28 11:00:48 +0100

    Updated build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.4.10"
+    kotlin("jvm") version "1.4.31"
     `java-library`
 }
 group = "nl.stanroelofs"
@@ -10,9 +10,22 @@ version = "1.0"
 repositories {
     mavenCentral()
 }
+
 dependencies {
     testImplementation(kotlin("test-junit"))
 }
+
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.jar {
+    manifest {
+        attributes(mapOf("Implementation-Title" to project.name,
+                         "Implementation-Version" to project.version))
+    }
+}
+
+java {
+    withSourcesJar()
 }

402ecc1917c07ee989f1bc1ad7b9b4916c722bd3
Stan <stan@stanroelofs.nl>
2021-03-09 12:47:59 +0100

    Check whether logging is enabled for a certain level before writing the output

--- src/main/kotlin/nl/stanroelofs/minilog/logger/Logger.kt
+++ src/main/kotlin/nl/stanroelofs/minilog/logger/Logger.kt
@@ -5,20 +5,34 @@ import nl.stanroelofs.minilog.handler.Writer
 
 class Logger(private val tag: String, private val formatter : Formatter, private val handler : Writer) : ILogger {
 
+    override var currentLevel = Level.OFF
+
     private fun log(level: Level, message: String) {
         handler.writeLog(tag, level, formatter.format(tag, level, message))
     }
 
-    override fun debug(message: String) = log(Level.DEBUG, message)
+    override fun debug(message: String){
+        if (debugEnabled)
+            log(Level.DEBUG, message)
+    }
     override fun d(message: String) = debug(message)
 
-    override fun info(message: String) = log(Level.INFO, message)
+    override fun info(message: String) {
+        if (infoEnabled)
+            log(Level.INFO, message)
+    }
     override fun i(message: String) = info(message)
 
-    override fun warning(message: String) = log(Level.WARNING, message)
+    override fun warning(message: String) {
+        if (warningEnabled)
+            log(Level.WARNING, message)
+    }
     override fun w(message: String) = warning(message)
 
-    override fun error(message: String) = log(Level.ERROR, message)
+    override fun error(message: String) {
+        if (errorEnabled)
+            log(Level.ERROR, message)
+    }
     override fun e(message: String) = error(message)
 }
 

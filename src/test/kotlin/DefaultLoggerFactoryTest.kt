24e4d65612c6cf8c6a0216a88a52ed1898cab243
Stan <stan@stanroelofs.nl>
2021-03-14 10:57:04 +0100

    Added tests for DefaultLoggerFactory

--- src/test/kotlin/DefaultLoggerFactoryTest.kt
+++ src/test/kotlin/DefaultLoggerFactoryTest.kt
@@ -1,8 +1,12 @@
 import nl.stanroelofs.minilog.DefaultLoggerFactory
+import nl.stanroelofs.minilog.formatter.Formatter
+import nl.stanroelofs.minilog.logger.Level
+import nl.stanroelofs.minilog.writer.Writer
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
+import kotlin.test.assertNotEquals
 
 class DefaultLoggerFactoryTest {
 
@@ -37,5 +41,34 @@ class DefaultLoggerFactoryTest {
         val logger = factory.getLogger("test")
         val logger2 = factory.getLogger("test")
         assertEquals(logger, logger2)
+    }
+
+    @Test
+    fun setWriter() {
+        val customWriter = object : Writer {
+            override fun writeLog(formattedMessage: String) {
+            }
+        }
+
+        val logger = factory.getLogger("test")
+        factory.defaultWriter = customWriter
+        assertNotEquals(logger.writer, customWriter)
+
+        assertEquals(customWriter, factory.getLogger("test2").writer)
+    }
+
+    @Test
+    fun setFormatter() {
+        val customFormatter = object : Formatter {
+            override fun format(name: String, level: Level, message: String): String {
+                return ""
+            }
+        }
+
+        val logger = factory.getLogger("test")
+        factory.defaultFormatter = customFormatter
+        assertNotEquals(logger.formatter, customFormatter)
+
+        assertEquals(customFormatter, factory.getLogger("test2").formatter)
     }
 }

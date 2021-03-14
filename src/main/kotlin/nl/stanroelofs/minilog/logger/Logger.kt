7c60e8df82523001aa197efb283762d0314eaba2
Stan <stan@stanroelofs.nl>
2021-03-14 10:20:33 +0100

    Removed ILogger

--- src/main/kotlin/nl/stanroelofs/minilog/logger/Logger.kt
+++ src/main/kotlin/nl/stanroelofs/minilog/logger/Logger.kt
@@ -1,39 +1,101 @@
 package nl.stanroelofs.minilog.logger
 
 import nl.stanroelofs.minilog.formatter.Formatter
-import nl.stanroelofs.minilog.handler.Writer
+import nl.stanroelofs.minilog.writer.Writer
 
-class Logger(private val tag: String, private val formatter : Formatter, private val handler : Writer) : ILogger {
+/**
+ * A logger object which
+ *
+ * @param name a name for this logger
+ * @param writer an object implementing the [Writer] interface which will be used to write log messages to an output
+ * @param formatter an object implementing the [Formatter] interface which will be used to format log messages
+ *
+ * @author Stan Roelofs
+ * @version 1.0
+ */
+class Logger(val name: String, private val writer: Writer, private val formatter: Formatter) {
 
-    override var level = Level.OFF
+    /** The current logging level for this logger
+     * @see Level
+     */
+    var level: Level = Level.OFF
 
-    private fun log(level: Level, message: String) {
-        handler.writeLog(tag, level, formatter.format(tag, level, message))
+    /**
+     * Returns whether logging with [level] is enabled
+     *
+     * @param level the logging level
+     * @return whether this logger is enabled for the given level
+     * @see Level
+     */
+    fun levelEnabled(level: Level) : Boolean = level >= this.level
+
+    val debugEnabled
+        /** Returns whether debug messages are enabled for this logger */
+        get() = levelEnabled(Level.DEBUG)
+
+    val infoEnabled
+        /** Returns whether info messages are enabled for this logger */
+        get() = levelEnabled(Level.INFO)
+
+    val warningEnabled
+        /** Returns whether warning messages are enabled for this logger */
+        get() = levelEnabled(Level.WARNING)
+
+    val errorEnabled
+        /** Returns whether error messages are enabled for this logger */
+        get() = levelEnabled(Level.ERROR)
+
+
+    fun log(level: Level, message: String) {
+        writer.writeLog(formatter.format(name, level, message))
     }
 
-    override fun debug(message: String){
+    /** Writes a debug message to some output
+     *
+     * @param message the log message
+     */
+    fun debug(message: String) {
         if (debugEnabled)
             log(Level.DEBUG, message)
     }
-    override fun d(message: String) = debug(message)
 
-    override fun info(message: String) {
+    /** Alias for [debug] */
+    fun d(message: String) = debug(message)
+
+    /** Writes an info message to some output
+     *
+     * @param message the log message
+     */
+    fun info(message: String) {
         if (infoEnabled)
             log(Level.INFO, message)
     }
-    override fun i(message: String) = info(message)
 
-    override fun warning(message: String) {
+    /** Alias for [info] */
+    fun i(message: String) = info(message)
+
+    /** Writes a warning message to some output
+     *
+     * @param message the log message
+     */
+    fun warning(message: String) {
         if (warningEnabled)
             log(Level.WARNING, message)
     }
-    override fun w(message: String) = warning(message)
 
-    override fun error(message: String) {
+    /** Alias for [warning] */
+    fun w(message: String) = warning(message)
+
+    /** Writes an error message to some output
+     *
+     * @param message the log message
+     * @see i
+     */
+    fun error(message: String) {
         if (errorEnabled)
             log(Level.ERROR, message)
     }
-    override fun e(message: String) = error(message)
+
+    /** Alias for [error] */
+    fun e(message: String) = error(message)
 }
-
-

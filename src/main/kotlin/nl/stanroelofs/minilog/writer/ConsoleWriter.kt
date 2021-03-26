10b1ea78c09841ae828a242af52a5806ca371bfd
Stan <stan@stanroelofs.nl>
2021-03-26 13:13:13 +0100

    Added open() and close() functions to the Writer interface

--- src/main/kotlin/nl/stanroelofs/minilog/writer/ConsoleWriter.kt
+++ src/main/kotlin/nl/stanroelofs/minilog/writer/ConsoleWriter.kt
@@ -1,7 +1,13 @@
 package nl.stanroelofs.minilog.writer
 
 class ConsoleWriter : Writer {
+    override fun open() {
+    }
+
     override fun writeLog(formattedMessage: String) {
         println(formattedMessage)
+    }
+
+    override fun close() {
     }
 }
--- src/main/kotlin/nl/stanroelofs/minilog/writer/Writer.kt
+++ src/main/kotlin/nl/stanroelofs/minilog/writer/Writer.kt
@@ -4,5 +4,10 @@ package nl.stanroelofs.minilog.writer
  * Interface for an object that handles logging output.
  */
 interface Writer {
+    //TODO
+    fun open()
+    // TODO
     fun writeLog(formattedMessage: String)
+    // TODO
+    fun close()
 }

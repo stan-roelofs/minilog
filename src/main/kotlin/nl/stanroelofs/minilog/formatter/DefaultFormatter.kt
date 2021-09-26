cbe2e9071ab7645d0f54ed63537c03c8d4a93821
Stan <stan@stanroelofs.nl>
2021-09-26 15:32:23 +0200

    Moved dateTime and thread getters to DefaultFormatter

--- src/main/kotlin/nl/stanroelofs/minilog/formatter/DefaultFormatter.kt
+++ src/main/kotlin/nl/stanroelofs/minilog/formatter/DefaultFormatter.kt
@@ -1,7 +1,20 @@
 package nl.stanroelofs.minilog.formatter
 
+import java.time.LocalDateTime
+
 class DefaultFormatter : Formatter {
+
+    val dateTime: LocalDateTime
+        get() {
+            return LocalDateTime.now()
+        }
+
+    val thread: String
+        get() {
+            return Thread.currentThread().name
+        }
+
     override fun format(message: LogMessage): String {
-        return "${message.dateTime} [${message.thread}] - ${message.tag} (${message.level}): ${message.message}"
+        return "$dateTime [$thread] - ${message.tag} (${message.level}): ${message.message}"
     }
 }
--- src/main/kotlin/nl/stanroelofs/minilog/formatter/LogMessage.kt
+++ src/main/kotlin/nl/stanroelofs/minilog/formatter/LogMessage.kt
@@ -1,16 +1,5 @@
 package nl.stanroelofs.minilog.formatter
 
 import nl.stanroelofs.minilog.logger.Level
-import java.time.LocalDateTime
 
-class LogMessage(val tag: String, val level: Level, val message: String) {
-    val dateTime: LocalDateTime
-        get() {
-            return LocalDateTime.now()
-        }
-
-    val thread: String
-        get() {
-            return Thread.currentThread().name
-        }
-}
+data class LogMessage(val tag: String, val level: Level, val message: String)
